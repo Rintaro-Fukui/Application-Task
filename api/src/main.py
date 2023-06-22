9f3f319b6f2f1e268347fa28e00815adeef4ea6d
Rintaro-Fukui <zrdf813@icloud.com>
2023-06-22 16:14:42 +0900

    add index.html

---
+++ api/src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# 6/21 課題1
+# 任意の数を足し合わせる
+@app.get("/addition")
+async def addition(n1:str, n2:str):
+    result = float(n1) + float(n2)
+    return {
+        "input":[{"n1": n1, "n2": n2}],
+        "output":[{"result": result}],
+    }
