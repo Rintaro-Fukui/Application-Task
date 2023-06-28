bb8a944ad5d88e95e582e7d69e890fadcf10907b
Rintaro-Fukui <zrdf813@icloud.com>
2023-06-28 14:14:16 +0900

    edit home-page.js

--- front/application-task/src/app/page.js
+++ front/application-task/src/app/page.js
@@ -26,6 +26,22 @@ export default function Home() {
         <Link href="/">
           <div className="tile is-parent">
             <div className="tile is-child box">
+              <p className="subtitle">06/28 課題1</p>
+            </div>
+          </div>
+        </Link>
+        <br />
+        <Link href="/">
+          <div className="tile is-parent">
+            <div className="tile is-child box">
+              <p className="subtitle">06/28 課題3</p>
+            </div>
+          </div>
+        </Link>
+        <br />
+        <Link href="/">
+          <div className="tile is-parent">
+            <div className="tile is-child box">
               <p className="subtitle">coming soon...</p>
             </div>
           </div>
